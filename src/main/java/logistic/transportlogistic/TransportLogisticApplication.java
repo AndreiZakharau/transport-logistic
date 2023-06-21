b74ea90f795a062500b1de40e6de118cd014a1a6
AndreyZ <a.zahar3111981@gmail.com>
2023-06-21 15:11:57 +0300

    start project

---
+++ src/main/java/logistic/transportlogistic/TransportLogisticApplication.java
@@ -0,0 +1,13 @@
+package logistic.transportlogistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportLogisticApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TransportLogisticApplication.class, args);
+  }
+
+}
---
+++ src/test/java/logistic/transportlogistic/TransportLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package logistic.transportlogistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportLogisticApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
