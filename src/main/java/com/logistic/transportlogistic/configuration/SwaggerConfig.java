2ac8854984ccb0e1e3b2846b7d006b7110ed7383
AndreyZ <a.zahar3111981@gmail.com>
2023-06-29 19:51:25 +0300

    updated SwaggerConfig

--- src/main/java/com/logistic/transportlogistic/configuration/SwaggerConfig.java
+++ src/main/java/com/logistic/transportlogistic/configuration/SwaggerConfig.java
@@ -4,20 +4,12 @@ package com.logistic.transportlogistic.configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(enforceUniqueMethods = false)
 public class SwaggerConfig {
 
-  @Bean
-  public GroupedOpenApi customOpenApi() {
-    return GroupedOpenApi.builder()
-        .group("com.logistic")
-        .pathsToMatch("/users/**")
-        .build();
-  }
 
   @Bean
   public OpenAPI customOpenAPI() {
